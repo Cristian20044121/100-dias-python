7d17045e8647780ac0d86552f1bafdbae404dbbf
Cristian <crdcaro01@gmail.com>
2024-04-09 15:39:37 -0500

    solucion día 09

--- 1-10/dia_09.py
+++ 1-10/dia_09.py
@@ -4,3 +4,11 @@
 Evalúa una expresión para ver si un número es mayor que otro
                 e imprime el resultado
 """
+num1 = 20
+num2 = 110
+
+resultado = num1 > num2
+if resultado:
+    print("num1 es mayor ", num1)
+else:
+    print("num 2 es mayor", num2)
