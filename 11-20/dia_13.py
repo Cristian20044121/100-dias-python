130ea299afd8946f1bdf01c0a7b5c57631984dcb
crdcaro01@gmail.com <crdcaro01@gmail.com>
2024-04-16 14:12:07 -0500

    solucion día 13

--- 11-20/dia_13.py
+++ 11-20/dia_13.py
@@ -4,3 +4,10 @@
 Declara dos variables booleanas, evalúa una expresión lógica
     usando dos operadores lógicos e imprime el resultado.
 """
+
+boolean1 = True
+boolean2 = False
+resultado = boolean1 and boolean2
+
+print(resultado)
+
