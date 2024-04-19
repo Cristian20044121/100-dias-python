9fb49ddb90bf6fa02baf5c0b2ce9f98152aa9d4a
Cristian <crdcaro01@gmail.com>
2024-04-18 20:23:53 -0500

    solucion día 15

--- 11-20/dia_15.py
+++ 11-20/dia_15.py
@@ -4,3 +4,14 @@
 Declara una variable de tipo cadena e imprime los últimos tres
                 carácter de la cadena sin usar ciclos.
 """
+cadena = "Hola"
+letra1 = cadena[0]
+letra2 = cadena[1]
+letra3 = cadena[2]
+resultado = letra1,letra2,letra3
+print(resultado)
+
+#resultado facil 
+#cadena = "Hola"
+#ultimos_tres = cadena[-3:]
+#print(ultimos_tres)
