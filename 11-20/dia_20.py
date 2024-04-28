df30eb9f4a446cd1d88d95a511d0aaa77e04fa11
Cristian <crdcaro01@gmail.com>
2024-04-27 23:23:01 -0500

    solucion día 20

--- 11-20/dia_20.py
+++ 11-20/dia_20.py
@@ -5,3 +5,10 @@
 Separa las mayúsculas y minúsculas sin usar ciclos en nuevas cadenas
             e imprime el resultado en una sola línea
 """
+
+cadena = 'PpYyTtHhOoNnIiSsTtAa'
+print(cadena[0:1]+cadena[2:3]+cadena[4:5]+cadena[6:7]+cadena[8:9]+cadena[10:11]+cadena[12:13]+cadena[14:15]+cadena[16:17]+cadena[18:19])
+
+#metodo rapido
+mayusculas = cadena[::2]
+print(mayusculas)
